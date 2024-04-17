9ab625112022ebe21df2e4dea1cf4caa127f9a6d
vitorbelluzzo <vitorbelluzzo@hotmail.com>
2024-04-17 20:17:29 -0300

    feat: Implemented basic form component in form.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,16 @@
+import { Form } from "./components/form";
 
 export function App() {
   return (
-    <div className="grid-cols-2 flex mx-auto justify-center items-center mt-36 gap-6">
-        <div className="bg-[#1E1E1E] px-9 rounded-2xl flex flex-col justify-center">
-          <h1 className="text-white text-base font-semibold mt-9 w-full  justify-center flex">Cadastro de usuários</h1>
-          <form action="">
-            <div className="flex flex-col gap-2  mt-4">
-            <span className="text-sm text-white">Nome</span>
-            <input type="text" className="bg-transparent border rounded-md"/>
-            </div>
-
-          </form>
-        </div>
+    <div className=" flex mx-auto justify-center items-center mt-36 gap-6">
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8">
+   <div className=" rounded-lg bg-[#1E1E1E] px-9  flex flex-col justify-center">
+    <span>Cadastro de usuários</span>
+   <Form />
+   </div>
+   <div className="rounded-lg bg-[#1E1E1E] px-9  flex flex-col justify-center">teste</div>
+</div>
+       
     
     </div>
    
---
+++ client/src/components/form.tsx
@@ -0,0 +1,9 @@
+export function Form() {
+  return (
+    <form action="">
+     <label htmlFor="">nome
+      <input type="text" name="" id="" className="bg-transparent border rounded" />
+     </label>
+    </form>
+  )
+}
