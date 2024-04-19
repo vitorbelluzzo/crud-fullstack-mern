0450720f1f5f5a8225a4bf988c69a67ea38019d3
vitorbelluzzo <vitorbelluzzo@hotmail.com>
2024-04-18 23:42:34 -0300

    feat: Expanded Form component with additional fields in form.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,21 @@ import { Form } from "./components/form";
 
 export function App() {
   return (
-    <div className=" flex mx-auto justify-center items-center mt-36 gap-6">
-      <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8">
-   <div className=" rounded-lg bg-[#1E1E1E] px-9  flex flex-col justify-center">
-    <span>Cadastro de usuários</span>
+    <div className=" flex mx-auto justify-center items-center mt-20 px-1 gap-6 ">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:gap-8 items-center">
+    
+    <div className=" rounded-lg bg-[#1E1E1E] flex flex-col justify-center w-96  px-10">
+    <span className="font-bold text-base mt-[39px] justify-center flex">Cadastro de usuários</span>
    <Form />
    </div>
-   <div className="rounded-lg bg-[#1E1E1E] px-9  flex flex-col justify-center">teste</div>
+   <div className="rounded-lg bg-[#1E1E1E] flex flex-col justify-center w-96  px-10">
+    <div className="">
+      <table>
+        
+      </table>
+    </div>
+    <div></div>
+   </div>
 </div>
        
     
--- client/src/components/form.tsx
+++ client/src/components/form.tsx
@@ -1,8 +1,21 @@
 export function Form() {
   return (
-    <form action="">
-     <label htmlFor="">nome
-      <input type="text" name="" id="" className="bg-transparent border rounded" />
+    <form action="" className="">
+     <label htmlFor="" className="flex flex-col mt-4 text-sm gap-2 font-semibold">Nome
+      <input type="text" name="" id="" className="bg-transparent border rounded-xl  p-4" />
+     </label>
+     <label htmlFor="" className="flex flex-col mt-4 text-sm gap-2 font-semibold">E-mail
+      <input type="email" name="" id="" className="bg-transparent border rounded-xl p-4" />
+     </label>
+     <label htmlFor="" className="flex flex-col mt-4 text-sm gap-2 font-semibold">Telefone
+      <input type="tel" name="Telefone" id="" className="bg-transparent border rounded-xl  p-4" />
+     </label>
+     <label htmlFor="" className="flex flex-col mt-4 text-sm gap-2 font-semibold ">Data de nascimento
+      <input type="date" name="" className="bg-transparent border rounded-xl p-4  " />
+     </label>
+
+     <label htmlFor="" className="flex justify-center mt-8  mb-6 ">
+     <button type="submit" className="border  justify-center p-4 rounded-2xl w-52 flex items-center">Salvar</button>
      </label>
     </form>
   )
